fa56a6b4e3db807d9a5953af791fbb52d8c12938
eritak <i.am.et1201@gmail.com>
2022-01-17 02:03:41 +0900

    index追加

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>PHP1回目課題</title>
+</head>
+
+<body>
+
+<h1>PHP01課題</h1>
+
+<ul>
+    <li><a href="input.php">情報登録画面</a></li>
+    <li><a href="write.php">登録内容一覧確認</a></li>
+    <li><a href="read.php">データ内容</a></li>
+</ul>
+
+</body>
+</html>
